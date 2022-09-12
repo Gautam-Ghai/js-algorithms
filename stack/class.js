6a11ebf2a987e38fb5a9c1cf898b80273786e5eb
Gautam Ghai <70984665+Gautam-Ghai@users.noreply.github.com>
2022-09-12 12:24:58 -0700

    Update class.js

--- stack/class.js
+++ stack/class.js
@@ -1,1 +1,30 @@
+class Stack {
+  constructor(){
+    this.count = 0;
+    this.data = {}
+  }
 
+  push(x) {
+    this.data[this.count] = x
+    this.count ++;
+  }
+
+  pop() {
+    if(this.count === 0) return undefined
+
+    this.count --;
+    var result = this.data[this.count]
+    delete this.data[this.count]
+
+    return result
+  }
+
+  size() {
+    return this.count
+  }
+
+  peek() {
+    return this.data[this.size - 1]
+  }
+  
+}
